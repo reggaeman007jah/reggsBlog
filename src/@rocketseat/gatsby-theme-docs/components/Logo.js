6d006f1f2fb9cfe278736001f7480a55d70aceb2
londondirt <londondirt@gmail.com>
2021-01-04 17:21:15 +0000

    pics

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Path to the logo file on your project
-import rocketseatLogo from 'assets/logo.png';
+import rocketseatLogo from './assets/logo3.jpg';
 
 const Logo = () => (
   <img src={rocketseatLogo} alt="Rocketseat logo" style={{ width: 180 }} />
